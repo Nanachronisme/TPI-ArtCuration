c1f7d19907b8cc5d3e8e75d80f45894fb4b84580
Risa <73257828+Nanachronisme@users.noreply.github.com>
2022-05-20 12:15:14 +0200

    Initial web routes
    basic routing for testing

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,11 +15,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/home', [HomeController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+Route::resource('artists', ArtistController::class);
+
+Route::resource('artworks', ArtistController::class);
+
